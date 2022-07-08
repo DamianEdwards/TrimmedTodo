6a54247d6ec6484910b3e64e7be3b5f03117601a
Damian Edwards <damian@damianedwards.com>
2022-07-08 10:50:41 -0700

    Fix script

--- Run-TodoApi.ps1
+++ Run-TodoApi.ps1
@@ -4,9 +4,9 @@ $projectName = "TrimmedTodo.MinimalApi.EfCore.Sqlite";
 $configKeyName = "dotnet-user-jwts:KeyMaterial";
 $userSecretsId = "7deba5fe-f06c-4a7d-9816-cdeccfc4e4a8";
 $userSecretsJsonPath = $env:APPDATA + "\Microsoft\UserSecrets\$userSecretsId\secrets.json";
-if (!Test-Path -Path $userSecretsJsonPath)
+if ((Test-Path -Path $userSecretsJsonPath) -ne $true)
 {
-    Write-Error "API project '$projectName' has not been initialized for JWT authentication. Please run 'dotnet user-jwts create' in the API project directory.";
+    Write-Error "API project '$projectName' has not been initialized for JWT authentication.`r`nPlease run 'dotnet user-jwts create' in the API project directory.";
     return;
 }
 $userSecrets = Get-Content -Path $userSecretsJsonPath | ConvertFrom-Json;
