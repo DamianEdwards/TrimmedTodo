4d67d93b9fc85fc39f2b6f21cdca20254fb2308d
Damian Edwards <damian@damianedwards.com>
2022-10-04 12:50:54 -0700

    rc.2 updates

--- scripts/Run-TodoApi.ps1
+++ scripts/Run-TodoApi.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop';
 
 # Set env var for JWT signing key
-$configKeyName = "dotnet-user-jwts:KeyMaterial";
+$configKeyName = "Authentication:Schemes:Bearer:SigningKeys";
 $userSecretsId = "7deba5fe-f06c-4a7d-9816-cdeccfc4e4a8";
 $userSecretsJsonPath = $env:APPDATA + "\Microsoft\UserSecrets\$userSecretsId\secrets.json";
 if ((Test-Path -Path $userSecretsJsonPath) -ne $true)
@@ -9,8 +9,22 @@ if ((Test-Path -Path $userSecretsJsonPath) -ne $true)
     Write-Error "API project '$projectName' has not been initialized for JWT authentication.`r`nPlease run 'dotnet user-jwts create' in the '$projectName' directory.";
     return;
 }
-$userSecrets = Get-Content -Path $userSecretsJsonPath | ConvertFrom-Json;
-$jwtSigningKey = $userSecrets | Select-Object -Property $configKeyName;
-$env:JWT_SIGNING_KEY = $jwtSigningKey.($configKeyName);
 
-.\scripts\Run-Project.ps1 -ProjectName $projectName
+$userSecretsJson = Get-Content -Path $userSecretsJsonPath | ConvertFrom-Json;
+#$userSecretsJson | Format-Table;
+
+$jwtSigningKeysArray = $userSecretsJson.($configKeyName);
+#Write-Host $jwtSigningKeysArray;
+
+$dotnetUserJwtsKeys = $jwtSigningKeysArray.Where({$_.("Issuer") -eq "dotnet-user-jwts"});
+#Write-Host $dotnetUserJwtsKeys;
+
+if ($dotnetUserJwtsKeys.Count -eq 1) {
+    $env:JWT_SIGNING_KEY = $dotnetUserJwtsKeys[0].("Value");
+    #Write-Host $dotnetUserJwtsKeys[0].("Value");
+
+    .\scripts\Run-Project.ps1 -ProjectName $projectName;
+} else {
+    Write-Error "Could not find signing key for issuer 'dotnet-user-jwts'";
+    return;
+}
